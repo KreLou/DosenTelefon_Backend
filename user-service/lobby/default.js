88f820ef10146cab6d6727159bb7b72e0a87c13d
Andreas Fiseni <info@amenzel.de>
2020-03-23 22:22:07 +0100

    added static stuff, removed domain informations because this is now manually configured in aws. Websocket code uses new communication protocol

--- user-service/lobby/default.js
+++ user-service/lobby/default.js
@@ -2,61 +2,130 @@
 
 const uuid = require('uuid');
 const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
-var ses = new AWS.SES({region: 'eu-west-1'});
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 module.exports.defaultHandler = (event, context, callback) => {
   const connectionId = event.requestContext.connectionId;
 
-  console.log("Hello world");
+  console.log("Default handler, got "+connectionId+" to dynamoDB");
+
   const timestamp = new Date().getTime();
   const data = JSON.parse(event.body);
-
-  console.log("Default handler, got "+connectionId+" to dynamoDB");
-//TODO: verify token
-  const params = {
-    TableName: process.env.CONNECT_TABLE,
-    Key: {
-      connectId: connectionId,
-    },
-    ExpressionAttributeNames: {
-      '#user': 'user'
-    },
-    ExpressionAttributeValues: {
-      ':user': data.uuid,
-      ':updatedAt': timestamp
-    },
-    UpdateExpression: 'SET ' +//
-                        '#user = :user, ' +//
-                        'updatedAt = :updatedAt',
-    ReturnValues: 'ALL_NEW',
-  };
-
-  // update the todo in the database
-  dynamoDb.update(params, (error, result) => {
-    // handle potential errors
-    if (error) {
-      console.error(error);
+  //TODO: verify token
+  console.log(data);
+  if (typeof data.version !== 'number' || data.version !== 1) {
+      console.error('incorrect message');
       callback(null, {
-        statusCode: error.statusCode || 501,
-        headers: {   'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Credentials': true,
-          'Content-Type': 'text/plain' },
-        body: 'Couldn\'t update connection.\n'+error.message
-        +'\n fnVersion: '+context.functionVersion
-        +'\n functionName: '+context.functionName,
+        statusCode: 400,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Message invalid.',
       });
-      console.log("Default handler, got "+connectionId+" to dynamoDB");
-
       return;
-    }
-  });
+  }
 
-  const response = {
-    statusCode: 200,
-    body: 'connection updated'
-  };
-  callback(null, response);
+  switch (data.event){
+    case "link_user":
+
+
+      const params = {
+        TableName: process.env.CONNECT_TABLE,
+        Key: {
+          connectId: connectionId,
+        },
+        ExpressionAttributeNames: {
+          '#user': 'user'
+        },
+        ExpressionAttributeValues: {
+          ':user': data.body.uuid,
+          ':updatedAt': timestamp
+        },
+        UpdateExpression: 'SET ' +//
+                            '#user = :user, ' +//
+                            'updatedAt = :updatedAt',
+        ReturnValues: 'ALL_NEW',
+      };
+
+      // update the todo in the database
+      dynamoDb.update(params, (error, result) => {
+        // handle potential errors
+        if (error) {
+          console.error(error);
+          callback(null, {
+            statusCode: error.statusCode || 501,
+            headers: {   'Access-Control-Allow-Origin': '*',
+              'Access-Control-Allow-Credentials': true,
+              'Content-Type': 'text/plain' },
+            body: 'Couldn\'t update connection.\n'+error.message
+            +'\n fnVersion: '+context.functionVersion
+            +'\n functionName: '+context.functionName,
+          });
+          console.log("Default handler, saved "+connectionId+" to dynamoDB");
+
+          return;
+        }
+      });
+
+      let connectionData;
+      console.log("sending response");
+      const apigwManagementApi = new AWS.ApiGatewayManagementApi({
+        apiVersion: '2018-11-29',
+        endpoint: event.requestContext.domainName
+      });
+
+      const postData = {
+      "version" : 1,
+      "event" : "call_request",
+      "body" : {
+          "openrainbow-details": {
+            //tbd
+          },
+          "username": "Merkel"
+        }
+      };
+
+
+
+      try {
+        let send = undefined;
+
+         send = async (connectionId, postData) => {
+         await apigwManagementApi.postToConnection({ ConnectionId: connectionId, Data: JSON.stringify(postData)  }).promise();
+         }
+         send(connectionId, postData);
+      } catch (e) {
+        if (e.statusCode === 410) {
+          console.log(`Found stale connection, deleting ${connectionId}`);
+          callback(null, {
+            statusCode: 410,
+            headers: { 'Content-Type': 'text/plain' },
+            body: `Found stale connection, deleting ${connectionId}`,
+          });
+        } else {
+          throw e;
+        }
+      }
+//TODO make code nicer https://github.com/aws-samples/simple-websockets-chat-app/blob/master/sendmessage/app.js
+
+      callback(null, {
+        statusCode: 200,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Data sent.',
+      });
+      break;
+    default:
+    var errorMsg = 'Unkown event '+data.event;
+    console.error(errorMsg);
+    callback(null, {
+      statusCode: 400,
+      headers: { 'Content-Type': 'text/plain' },
+      body: errorMsg,
+    });
+    return;
+  }
+
+
+
+
 
 };
--- user-service/users/update.js
+++ user-service/users/update.js
@@ -24,7 +24,9 @@ module.exports.update = (event, context, callback) => {
     Key: {
       uuid: event.pathParameters.userId,
     },
-
+    ExpressionAttributeNames: {
+      '#token': 'token'
+    },
     ExpressionAttributeValues: {
       ':username': data.username,
       ':topicsOK': data.topicsOK,
@@ -34,6 +36,8 @@ module.exports.update = (event, context, callback) => {
     },
     UpdateExpression: 'SET ' +//
                         'username = :username, ' +//
+                        'email = :email, ' +//
+                        
                         'topicsOK = :topicsOK, ' +//
                         'topicsNotOK = :topicsNotOK, ' +//
                         'active = :active, ' +//
@@ -48,9 +52,7 @@ module.exports.update = (event, context, callback) => {
       console.error(error);
       callback(null, {
         statusCode: error.statusCode || 501,
-        headers: {   'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Credentials': true,
-          'Content-Type': 'text/plain' },
+        headers: { 'Content-Type': 'text/plain' },
         body: 'Couldn\'t fetch the user.\n'+error.message
         +'\n fnVersion: '+context.functionVersion
         +'\n functionName: '+context.functionName,
